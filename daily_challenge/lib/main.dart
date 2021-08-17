c1e35954609cc8bb1be32cea1e84aa3e6549719f
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-08-17 09:54:37 +0400

    challenge1

---
+++ daily_challenge/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(backgroundColor: Colors.blue,
+          appBar: AppBar(backgroundColor: Colors.lightBlue,
+            title: Text('Hello'),
+          ),
+        ),
+      ),
+    );
