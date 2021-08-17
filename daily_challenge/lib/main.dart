e6d12b262302448fa4900d51b866bc42b4cc599e
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-08-17 11:18:25 +0400

    Challenge2

--- daily_challenge/lib/main.dart
+++ daily_challenge/lib/main.dart
@@ -3,15 +3,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-            backgroundColor: Colors.blue,
-            appBar: AppBar(
-              backgroundColor: Colors.lightBlue,
-              title: Text('Hello'),
-            ),
-            body: Center(
-              child: Image(
-                  image: NetworkImage(
-                      'https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1057506940?k=6&m=1057506940&s=612x612&w=0&h=C11yA-ESqeuCX63QkRpPyWmAMXJJvZw0niQluGnATlI=')),
-            )),
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            backgroundColor: Colors.lightBlue,
+            title: Text('Hello'),
+          ),
+          body: Center(
+            child: Image(image: AssetImage('images/diamond.jpg')),
+          ),
+        ),
       ),
     );
