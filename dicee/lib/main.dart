2dfb8ec0757e84ecc880b3fe6991f799b3fa39ac
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-08-19 23:50:35 +0400

    test

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,35 +16,36 @@ void main() {
     ),
   );
 }
+
 class DicePage extends StatefulWidget {
   @override
   _DicePageState createState() => _DicePageState();
 }
 
 class _DicePageState extends State<DicePage> {
-  int leftDiceNumber=1;
-  int rightDiceNumber=2;
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-
     return Center(
       child: Row(
         children: [
           Expanded(
             child: FlatButton(
-                onPressed: () {setState(() {leftDiceNumber=5;
-
-                });
-
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber = Random().nextInt(6)+1;
+                  });
                 },
                 child: Image.asset('images/dice$leftDiceNumber.png')),
           ),
           Expanded(
             child: FlatButton(
                 onPressed: () {
-             setState(() {
-               rightDiceNumber=3;
-             });
+                  setState(() {
+                    rightDiceNumber = Random().nextInt(6)+1;
+                  });
                 },
                 child: Image.asset('images/dice$rightDiceNumber.png')),
           )
@@ -51,4 +54,3 @@ class _DicePageState extends State<DicePage> {
     );
   }
 }
-
