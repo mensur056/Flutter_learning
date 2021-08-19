64b75c8c99886bc1a8e7680948cb517785029697
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-08-19 09:56:38 +0400

    first app

--- micard/lib/main.dart
+++ micard/lib/main.dart
@@ -1,70 +1,82 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MiCard());
+void main() {
+  runApp(MiCard());
+}
 
 class MiCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
+              children: <Widget>[
                 CircleAvatar(
-                  radius: 50,
+                  radius: 50.0,
                   backgroundImage: AssetImage('images/mensur.jpg'),
                 ),
                 Text(
                   'Mensur Serxanov',
-                  style: TextStyle(fontFamily: 'Pacifico', fontSize: 25),
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
                 Text(
-                  'Flutter Developer',
-                  style: TextStyle(fontSize: 15, fontFamily: 'Source'),
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Source',
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
                 SizedBox(
-                  height: 10,
-                  width: 150,
+                  height: 20.0,
+                  width: 150.0,
                   child: Divider(
-                    color: Colors.teal,
-                    height: 10,
+                    color: Colors.teal.shade100,
                   ),
                 ),
                 Card(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.phone,
-                      color: Colors.green,
-                    ),
-                    title: Text(
-                      '+994 50 645 29 39',
-                      style: TextStyle(fontSize: 20.0, fontFamily: 'Source'),
-                    ),
-                  ),
-                ),
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        '+994 50 645 29 39',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    )),
                 Card(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      'Mensur.Serxanov@gmail.com',
-                      style: TextStyle(fontFamily: 'Source', fontSize: 20),
-                    ),
-                  ),
-                )
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'Mensur.Serxanov@gmail.com',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source'),
+                      ),
+                    ))
               ],
-            ),
-          ),
-        ),
+            )),
       ),
     );
   }
