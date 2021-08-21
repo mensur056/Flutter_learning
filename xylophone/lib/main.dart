bad29efa0d886ece114387b5ae4655277179bad9
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-08-21 23:42:28 +0400

    test

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -9,13 +9,13 @@ class Xylophone extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
-  Expanded buildKey({Color? color,int? soundNumber}) {
+  Expanded buildKey({Color? color,int? soundNumber,String? text}) {
     return Expanded(
       child: FlatButton(color: color,
         onPressed: () {
           PlaySound(soundNumber!);
         },
-        child: Text('Click me'),
+        child: Text(text!),
       ),
     );
 
@@ -32,13 +32,13 @@ class Xylophone extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
 
-                buildKey(color: Colors.red,soundNumber: 1),
-                buildKey(color: Colors.green,soundNumber: 2),
-                buildKey(color: Colors.blue,soundNumber: 3),
-                buildKey(color: Colors.yellow,soundNumber: 4),
-                buildKey(color: Colors.teal,soundNumber: 5),
-                buildKey(color: Colors.orange,soundNumber: 6),
-                buildKey(color: Colors.pink,soundNumber: 7),
+                buildKey(color: Colors.red,soundNumber: 1,text: 'Music1'),
+                buildKey(color: Colors.green,soundNumber: 2,text: 'Music2'),
+                buildKey(color: Colors.blue,soundNumber: 3,text: 'Music3'),
+                buildKey(color: Colors.yellow,soundNumber: 4,text: 'Music4'),
+                buildKey(color: Colors.teal,soundNumber: 5,text: 'Music5'),
+                buildKey(color: Colors.orange,soundNumber: 6,text: 'Music6'),
+                buildKey(color: Colors.pink,soundNumber: 7,text: 'Music7'),
 
 
               ],
