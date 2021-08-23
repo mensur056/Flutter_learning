252f1a409ec20086797bc95d75d513bb115ab9a3
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-08-23 09:39:04 +0400

    test

--- quizz_app/lib/main.dart
+++ quizz_app/lib/main.dart
@@ -25,13 +25,13 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(Icons.close, color: Colors.red)
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
+  int questionNumber=0;
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+               questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -93,6 +93,13 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                setState(
+                  () {
+                    scoreKeeper.add(
+                      Icon(Icons.close, color: Colors.red),
+                    );
+                  },
+                );
                 //The user picked false.
               },
             ),
@@ -105,3 +112,8 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
