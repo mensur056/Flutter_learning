0bd495926b9576becc0594cd24446d325d83450e
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-08-24 09:09:25 +0400

    test

--- quizz_app/lib/main.dart
+++ quizz_app/lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
-
+import './const.dart';
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.indigo[700],
-            body: SafeArea(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  child: QuizzPage(),
-                ))));
+      home: Scaffold(
+        backgroundColor: Colors.indigo[700],
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizzPage(),
+          ),
+        ),
+      ),
+    );
   }
 }
 
@@ -22,6 +25,8 @@ class QuizzPage extends StatefulWidget {
 }
 
 class _QuizzPageState extends State<QuizzPage> {
+  List<Widget> elections = [];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -44,44 +49,58 @@ class _QuizzPageState extends State<QuizzPage> {
             ),
           ),
         ),
-        Row(children: [Icon(Icons.mood,color:Colors.green),
-        Icon(Icons.mood_bad,color:Colors.red)],),
+        Row(
+          children: elections,
+        ),
         Expanded(
           flex: 1,
           child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 6.0),
-              child: Row(children: <Widget>[
+            padding: EdgeInsets.symmetric(horizontal: 6.0),
+            child: Row(
+              children: <Widget>[
                 Expanded(
-                    child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 6),
-                        child: RaisedButton(
-                          padding: EdgeInsets.all(12),
-                          textColor: Colors.white,
-                          color: Colors.red[400],
-                          child: Icon(
-                            Icons.close,
-                            size: 30.0,
-                          ),
-                          onPressed: () {},
-                        ))),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 6),
+                    child: RaisedButton(
+                      padding: EdgeInsets.all(12),
+                      textColor: Colors.white,
+                      color: Colors.red[400],
+                      child: Icon(
+                        Icons.close,
+                        size: 30.0,
+                      ),
+                      onPressed: () {setState(() {
+                        elections.add(kFalseIcon);
+                      });},
+                    ),
+                  ),
+                ),
                 Expanded(
-                    child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 6),
-                        child: RaisedButton(
-                          padding: EdgeInsets.all(12),
-                          textColor: Colors.white,
-                          color: Colors.green[400],
-                          child: Icon(Icons.check, size: 30.0),
-                          onPressed: () {},
-                        ))),
-              ])),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 6),
+                    child: RaisedButton(
+                      padding: EdgeInsets.all(12),
+                      textColor: Colors.white,
+                      color: Colors.green[400],
+                      child: Icon(Icons.check, size: 30.0),
+                      onPressed: () {setState(() {
+                        elections.add(kTrueIcon);
+                      });},
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         )
       ],
     );
   }
 }
+
 /*
 question1: 'You can lead a cow down stairs but not up stairs.', false,
 question2: 'Approximately one quarter of human bones are in the feet.', true,
 question3: 'A slug\'s blood is green.', true,
 */
+
