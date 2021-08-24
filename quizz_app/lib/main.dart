91f98daedb8cbde438d4f54bc48cf11a4abc3bf4
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-08-24 08:47:49 +0400

    test

--- quizz_app/lib/main.dart
+++ quizz_app/lib/main.dart
@@ -6,34 +6,22 @@ class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: QuizPage(),
-          ),
-        ),
-      ),
-    );
+        home: Scaffold(
+            backgroundColor: Colors.indigo[700],
+            body: SafeArea(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: QuizzPage(),
+                ))));
   }
 }
 
-class QuizPage extends StatefulWidget {
+class QuizzPage extends StatefulWidget {
   @override
-  _QuizPageState createState() => _QuizPageState();
+  _QuizzPageState createState() => _QuizzPageState();
 }
 
-class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
-  List<bool> answers = [false, true, true];
-  int questionNumber = 0;
-
+class _QuizzPageState extends State<QuizzPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -41,87 +29,52 @@ class _QuizPageState extends State<QuizPage> {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Expanded(
-          flex: 5,
+          flex: 4,
           child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                'Questions',
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
-              onPressed: () {
-                bool correctAnswer = answers[questionNumber];
-                if (correctAnswer == true) {
-                  print('user got it right');
-                } else {
-                  print('user got it wrong');
-                }
-                setState(() {
-                  questionNumber++;
-                  scoreKeeper.add(Icon(
-                    Icons.check,
-                    color: Colors.green,
-                  ));
-                });
-                //The user picked true.
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
                   fontSize: 20.0,
                   color: Colors.white,
                 ),
               ),
-              onPressed: () {
-                bool correctAnswer = answers[questionNumber];
-                if (correctAnswer == false) {
-                  print('user got it right');
-                } else {
-                  print('user got it wrong');
-                }
-                setState(
-                  () {
-                    questionNumber++;
-                    scoreKeeper.add(
-                      Icon(Icons.close, color: Colors.red),
-                    );
-                  },
-                );
-                //The user picked false.
-              },
             ),
           ),
         ),
-        Row(
-          children: scoreKeeper,
+        Row(children: [Icon(Icons.mood,color:Colors.green),
+        Icon(Icons.mood_bad,color:Colors.red)],),
+        Expanded(
+          flex: 1,
+          child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 6.0),
+              child: Row(children: <Widget>[
+                Expanded(
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 6),
+                        child: RaisedButton(
+                          padding: EdgeInsets.all(12),
+                          textColor: Colors.white,
+                          color: Colors.red[400],
+                          child: Icon(
+                            Icons.close,
+                            size: 30.0,
+                          ),
+                          onPressed: () {},
+                        ))),
+                Expanded(
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 6),
+                        child: RaisedButton(
+                          padding: EdgeInsets.all(12),
+                          textColor: Colors.white,
+                          color: Colors.green[400],
+                          child: Icon(Icons.check, size: 30.0),
+                          onPressed: () {},
+                        ))),
+              ])),
         )
       ],
     );
