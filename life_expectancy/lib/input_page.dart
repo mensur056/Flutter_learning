13f20a6975e3961bfc5883271527d0e0bdc05a40
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-08-29 22:19:39 +0400

    test

--- life_expectancy/lib/input_page.dart
+++ life_expectancy/lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
+import 'icon_content.dart';
+import 'MyContainer.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -65,49 +67,4 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class IconContent extends StatelessWidget {
-  IconContent({required this.icon, required this.label});
 
-  final IconData icon;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          icon,
-          size: 80.0,
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Text(
-          label,
-          style: TextStyle(
-              fontSize: 18, color: Color(0xFF8D8E98), fontFamily: 'Pacifico'),
-        )
-      ],
-    );
-  }
-}
-
-class MyContainer extends StatelessWidget {
-  MyContainer({required this.colour, required this.cardChild});
-
-  final Color colour;
-  final Widget cardChild;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: colour,
-      ),
-    );
-  }
-}
