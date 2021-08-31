3c0a11e3399e617fceccaf56103212c6aac638c6
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-08-31 20:10:26 +0400

    test

--- life_expectancy/lib/input_page.dart
+++ life_expectancy/lib/input_page.dart
@@ -95,19 +95,20 @@ class _InputPageState extends State<InputPage> {
                     ),
                     SliderTheme(
                       data: SliderTheme.of(context).copyWith(
+                        inactiveTickMarkColor: Color(0xFF8D8E98),
                           activeTrackColor: Colors.white,
                           thumbColor: Color(0xFFEB1555),
                           overlayColor: Color(0x29EB1555),
                           thumbShape:
                               RoundSliderThumbShape(enabledThumbRadius: 15),
                           overlayShape:
-                              RoundSliderOverlayShape(overlayRadius: 30.0)),
+                              RoundSliderOverlayShape(overlayRadius: 20.0)),
                       child: Slider(
                         value: height.toDouble(),
                         min: 120.0,
                         max: 220.0,
                         activeColor: Colors.white,
-                        inactiveColor: Color(0xFF8D8E98),
+
                         onChanged: (double newValue) {
                           setState(() {
                             setState(() {
--- life_expectancy/lib/main.dart
+++ life_expectancy/lib/main.dart
@@ -3,6 +3,8 @@ import 'input_page.dart';
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
