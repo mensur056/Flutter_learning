e3fa96026065ac9641b4e843fc27805d01804c40
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-09-05 09:42:54 +0400

    test

--- daily_challenge/lib/use_to_wigets/Stack/main.dart
+++ daily_challenge/lib/use_to_wigets/Stack/main.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
-void main()=>runApp(MyApp());
+
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home:StackLesson(),);
+    return MaterialApp(
+      home: StackLesson(),
+    );
   }
 }
+
 class StackLesson extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return  Center(child: Stack(children: [blueBox,pinkBox,star],),);
+    return Center(
+      child: Scaffold(backgroundColor: Colors.black,
+        body: Center(
+          child: Container(
+            width: 350,
+            height: 350,
+            child: Stack(
+              children: [blueBox, pinkBox, Positioned(bottom: 0,right:0, child: star)],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
-Widget blueBox=Container(color: Colors.blue,height: 200,width: 200,);
-Widget pinkBox=Container(color: Colors.pink,height: 100,width: 100,);
-Widget star=Icon(Icons.star,size: 50,color: Colors.yellow,);
+
+Widget blueBox = Container(
+  color: Colors.blue,
+  height: 200,
+  width: 200,
+);
+Widget pinkBox = Container(
+  color: Colors.pink,
+  height: 100,
+  width: 100,
+);
+Widget star = Icon(
+  Icons.star,
+  size: 50,
+  color: Colors.yellow,
+);
