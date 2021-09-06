31783e504ec7a22aa05c76811efac4cb20ff6a42
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-09-06 08:53:22 +0400

    test

---
+++ daily_challenge/lib/use_to_wigets/StateManagement/callBack.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('callback kullanımı'),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(child: SolWidgetA()),
+            Expanded(child: SagWidgetA())
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SolWidgetA extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.yellow,
+        child: Column(
+          children: [
+            Text(
+              'Sol Widget',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              'Sehir: ..... ',
+              style: TextStyle(fontSize: 20),
+            )
+          ],
+        ));
+  }
+}
+
+class SagWidgetA extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      child: Column(children: [
+        Text(
+          'SagWidget A',
+          style: TextStyle(fontSize: 20),
+        ),
+        SagWidgetB()
+      ]),
+    );
+  }
+}
+
+class SagWidgetB extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      width: 180,
+      color: Colors.purple,
+      child: Column(children: [
+        Text(
+          'SagWidget B',
+          style: TextStyle(fontSize: 20),
+        ),
+        SagWidgetC()
+      ]),
+    );
+  }
+}
+
+class SagWidgetC extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      height: 150,
+      width: 160,
+      child: Column(children: [
+        Text(
+          'SagWidget C',
+          style: TextStyle(fontSize: 20),
+        ),
+        Text(
+          'Şehir: ... ',
+          style: TextStyle(fontSize: 20),
+        ),
+        TextField(onChanged: null)
+      ]),
+    );
+  }
+}
