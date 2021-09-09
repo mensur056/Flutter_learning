946ad98b58c987bf8003a5fb808bae6e21d05e83
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-09-09 09:24:16 +0400

    test

--- fortune_teller_app/lib/main.dart
+++ fortune_teller_app/lib/main.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
-void main ()=>runApp(MyApp());
 
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(appBar: AppBar(title: Text(''),),),);
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.white,
+              displayColor: Colors.white,
+            ),
+      ),
+      home: Falci(),
+    );
+  }
+}
+
+class Falci extends StatefulWidget {
+  @override
+  _FalciState createState() => _FalciState();
+}
+
+class _FalciState extends State<Falci> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.pink,
+      appBar: AppBar(
+        title: Center(child: Text("Gunun Fali")),
+      ),body: Column(),
+    );
   }
 }
