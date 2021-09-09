bba291414470e0f2bb8b696d40657cebb9fc2388
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-09-09 08:52:59 +0400

    test

--- what_l_eat/lib/main.dart
+++ what_l_eat/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -30,16 +31,37 @@ class FoodPage extends StatefulWidget {
 
 class _FoodPageState extends State<FoodPage> {
   int soupNumber = 1;
-
   int eatNumber = 1;
-
   int sweetNumber = 1;
-  void changeFood(){setState(() {
-    soupNumber = Random().nextInt(5)+1;
-    eatNumber = Random().nextInt(5)+1;
-    sweetNumber = Random().nextInt(5)+1;
+  List<String> soupName = [
+    'Mercimek',
+    'Tarhana',
+    'Tavuksuyu',
+    'Dugun Corbasi',
+    'Yogutlu Corba'
+  ];
+  List<String> eatName = [
+    'Karnıyarık',
+    'Mantı',
+    'Kuru Fasulye',
+    'İçli Köfte',
+    'Izgara Balık'
+  ];
+  List<String> sweetName = [
+    'Kadayıf',
+    'Baklava',
+    'Sütlaç',
+    'Kazandibi',
+    'Dondurma'
+  ];
 
-  });}
+  void changeFood() {
+    setState(() {
+      soupNumber = Random().nextInt(5) + 1;
+      eatNumber = Random().nextInt(5) + 1;
+      sweetNumber = Random().nextInt(5) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,6 +77,18 @@ class _FoodPageState extends State<FoodPage> {
                 child: Image.asset('images/corba_$soupNumber.jpg')),
           ),
         ),
+        Text(
+          soupName[soupNumber - 1],
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        Container(
+            width: 200,
+            child: Divider(
+              height: 5,
+              color: Colors.black,
+            )),
         Expanded(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
@@ -65,16 +99,34 @@ class _FoodPageState extends State<FoodPage> {
                 child: Image.asset('images/yemek_$eatNumber.jpg')),
           ),
         ),
+        Text(
+          eatName[eatNumber - 1],
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        Container(
+            width: 200,
+            child: Divider(
+              height: 5,
+              color: Colors.black,
+            )),
         Expanded(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: FlatButton(
                 highlightColor: Colors.white,
                 splashColor: Colors.white,
-                onPressed:  changeFood,
+                onPressed: changeFood,
                 child: Image.asset('images/tatli_$sweetNumber.jpg')),
           ),
-        )
+        ),
+        Text(
+          sweetName[sweetNumber - 1],
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
       ]),
     );
   }
