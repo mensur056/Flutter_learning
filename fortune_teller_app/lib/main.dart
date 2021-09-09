3658fa28511b9f99c98f486c288d721329e3b611
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-09-09 20:35:51 +0400

    test

--- fortune_teller_app/lib/main.dart
+++ fortune_teller_app/lib/main.dart
@@ -25,10 +25,49 @@ class Falci extends StatefulWidget {
 class _FalciState extends State<Falci> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.pink,
+    return Scaffold(
+      backgroundColor: Colors.purpleAccent,
       appBar: AppBar(
         title: Center(child: Text("Gunun Fali")),
-      ),body: Column(),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(bottom: 20),
+            width: 150,
+            child: Image.asset(
+              'images/falci.png',
+            ),
+          ),
+          Card(margin: EdgeInsets.symmetric(vertical: 5,horizontal: 40),
+            child: ListTile(
+              leading: Icon(
+                Icons.favorite,
+                color: Colors.red,
+              ),title: Text('ASK DRUMU'),
+            ),
+          ),
+          Card(margin: EdgeInsets.symmetric(vertical: 5,horizontal: 40),
+            child: ListTile(
+              leading: Icon(
+                Icons.shop,
+                color: Colors.green,
+              ),title: Text('PARA DRUMU'
+            ),
+            ),
+          ),
+          Card(margin: EdgeInsets.symmetric(vertical: 5,horizontal: 40),
+            child: ListTile(
+              leading: Icon(
+                Icons.explore,
+                color: Colors.blue,
+              ),title: Text('GUNLUK TAVSIYE'),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
