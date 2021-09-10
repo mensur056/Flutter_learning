13e7bac9ed69186f76efc8e58f427bc93f7a2e4b
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-09-10 11:58:13 +0400

    test

--- weather_forecast/lib/HomePage.dart
+++ weather_forecast/lib/HomePage.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 
-
-
-
 class HomePage extends StatefulWidget {
-
-
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -13,6 +8,32 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(decoration: BoxDecoration(image: DecorationImage(image: AssetImage('images/c.jpg'))),);
+    return Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover, image: AssetImage('images/c.jpg'))),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '20° C',
+                  style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold),
+                ),
+                Row(mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Ankara',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.search))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
