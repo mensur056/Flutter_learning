d5e9a0074010e10fde1a651f10c925388a72e707
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-09-11 22:23:04 +0400

    My Second App

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -4,43 +4,56 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(Xylophone());
 
 class Xylophone extends StatelessWidget {
-  void PlaySound(int soundNumber) {
+  void playSound(int soundNumber) {
     final player = AudioCache();
     player.play('note$soundNumber.wav');
   }
 
-  Expanded buildKey({Color? color,int? soundNumber,String? text}) {
+  Expanded buildKey({Color? color, int? soundNumber, String? text}) {
     return Expanded(
-      child: FlatButton(color: color,
+      child: FlatButton(
+        color: color,
         onPressed: () {
-          PlaySound(soundNumber!);
+          playSound(soundNumber!);
         },
         child: Text(text!),
       ),
     );
-
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: ThemeData.dark(),
-      home: Scaffold(backgroundColor: Colors.black54,
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        backgroundColor: Colors.black54,
         body: SafeArea(
           child: Center(
-            child: Column(
-
+            child: Row(
               children: [
-                buildKey(color: Colors.red,soundNumber: 1,text: 'Music1'),
-                buildKey(color: Colors.green,soundNumber: 2,text: 'Music2'),
-                buildKey(color: Colors.blue,soundNumber: 3,text: 'Music3'),
-                buildKey(color: Colors.yellow,soundNumber: 4,text: 'Music4'),
-                buildKey(color: Colors.white,soundNumber: 5,text: 'Music5'),
-                buildKey(color: Colors.orange,soundNumber: 6,text: 'Music6'),
-                buildKey(color: Colors.pink,soundNumber: 7,text: 'Music7'),
-                buildKey(color: Colors.grey,soundNumber: 1,text: 'Music8'),
-
-
-
+                Column(
+                  children: [
+                    buildKey(color: Colors.red, soundNumber: 1, text: 'Music1'),
+                    buildKey(
+                        color: Colors.green, soundNumber: 2, text: 'Music2'),
+                    buildKey(
+                        color: Colors.blue, soundNumber: 3, text: 'Music3'),
+                    buildKey(
+                        color: Colors.yellow, soundNumber: 4, text: 'Music4'),
+                  ],
+                ),
+                Column(
+                  children: [
+                    buildKey(
+                        color: Colors.black, soundNumber: 5, text: 'Music5'),
+                    buildKey(
+                        color: Colors.orange, soundNumber: 6, text: 'Music6'),
+                    buildKey(
+                        color: Colors.pink, soundNumber: 7, text: 'Music7'),
+                    buildKey(
+                        color: Colors.grey, soundNumber: 1, text: 'Music8'),
+                  ],
+                )
               ],
             ),
           ),
