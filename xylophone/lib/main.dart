c3a2270e1b303ea62207e2f93b2ebc466f3d5f4b
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-09-11 22:31:31 +0400

    My Second App

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -29,32 +29,42 @@ class Xylophone extends StatelessWidget {
         backgroundColor: Colors.black54,
         body: SafeArea(
           child: Center(
-            child: Row(
-              children: [
-                Column(
-                  children: [
-                    buildKey(color: Colors.red, soundNumber: 1, text: 'Music1'),
-                    buildKey(
-                        color: Colors.green, soundNumber: 2, text: 'Music2'),
-                    buildKey(
-                        color: Colors.blue, soundNumber: 3, text: 'Music3'),
-                    buildKey(
-                        color: Colors.yellow, soundNumber: 4, text: 'Music4'),
-                  ],
-                ),
-                Column(
-                  children: [
-                    buildKey(
-                        color: Colors.black, soundNumber: 5, text: 'Music5'),
-                    buildKey(
-                        color: Colors.orange, soundNumber: 6, text: 'Music6'),
-                    buildKey(
-                        color: Colors.pink, soundNumber: 7, text: 'Music7'),
-                    buildKey(
-                        color: Colors.grey, soundNumber: 1, text: 'Music8'),
-                  ],
-                )
-              ],
+            child: Center(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        buildKey(
+                            color: Colors.red,
+                            soundNumber: 1,
+                            text: 'Music1'),
+                        buildKey(
+                            color: Colors.green, soundNumber: 2, text: 'Music2'),
+                        buildKey(
+                            color: Colors.blue, soundNumber: 3, text: 'Music3'),
+                        buildKey(
+                            color: Colors.yellow, soundNumber: 4, text: 'Music4'),
+                      ],
+                    ),
+                  ),
+
+                  Expanded(child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      buildKey(
+                          color: Colors.black, soundNumber: 5, text: 'Music5'),
+                      buildKey(
+                          color: Colors.orange, soundNumber: 6, text: 'Music6'),
+                      buildKey(
+                          color: Colors.pink, soundNumber: 7, text: 'Music7'),
+                      buildKey(
+                          color: Colors.grey, soundNumber: 1, text: 'Music8'),
+                    ],
+                  ))
+                ],
+              ),
             ),
           ),
         ),
