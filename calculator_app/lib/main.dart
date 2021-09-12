945ade6aded09d4b988abe5c41f02bc84078cbed
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-09-12 21:40:14 +0400

    test

---
+++ calculator_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyCalculator());
+
+class MyCalculator extends StatefulWidget {
+  @override
+  _MyCalculatorState createState() => _MyCalculatorState();
+}
+
+class _MyCalculatorState extends State<MyCalculator> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Calculator'),
+        ),body: Container(child: Column(children: [Row(children: [Container()],)],),),
+      ),
+    );
+  }
+}
