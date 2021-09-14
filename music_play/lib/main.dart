2ab81ee1aaf6e20dc39f39bd6a96b72f85ef1503
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-09-14 10:19:25 +0400

    test

---
+++ music_play/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MusicApp(),
+    );
+  }
+}
+
+class MusicApp extends StatefulWidget {
+  @override
+  _MusicAppState createState() => _MusicAppState();
+}
+
+class _MusicAppState extends State<MusicApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.blue, Colors.blueAccent]),
+        ),
+      ),
+    );
+  }
+}
