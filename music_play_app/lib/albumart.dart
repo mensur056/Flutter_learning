e45877eb93c306c5b31a578beb21f11453db0503
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-09-17 10:11:55 +0400

    test

--- music_play_app/lib/albumart.dart
+++ music_play_app/lib/albumart.dart
@@ -4,7 +4,8 @@ class AlbumArt extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 260,width: 260,
+      height: 260,width: 260,margin: EdgeInsets.symmetric(vertical: 40,horizontal: 20),
+      child: Image.asset('images/img.png'),
     );
   }
 }
