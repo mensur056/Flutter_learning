914879e5ff5ec16fb743f464a4ab28bb9f3b8af0
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-09-18 09:24:31 +0400

    test

--- music_play_app/lib/PlayerControls.dart
+++ music_play_app/lib/PlayerControls.dart
@@ -6,7 +6,7 @@ class PlayerControls extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Controls(
             icon: Icons.repeat,
@@ -30,7 +30,26 @@ class PlayerControls extends StatelessWidget {
 class PlayControl extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 100,
+      width: 100,
+      decoration: BoxDecoration(
+        color: primaryColor,
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+              color: darkPrimaryColor.withOpacity(0.5),
+              offset: Offset(20, 8),
+              spreadRadius: 3,
+              blurRadius: 25),
+          BoxShadow(
+              color: Colors.white,
+              offset: Offset(-3, -4),
+              spreadRadius: -2,
+              blurRadius: 20)
+        ],
+      ),
+    );
   }
 }
 
@@ -42,8 +61,8 @@ class Controls extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 50,
-      width: 50,
+      height: 60,
+      width: 60,
       decoration: BoxDecoration(
         color: primaryColor,
         shape: BoxShape.circle,
