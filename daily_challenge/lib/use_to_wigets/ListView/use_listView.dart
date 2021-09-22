f93ca810a6781bd80b83494de8c987b92fad0f21
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-09-22 19:35:45 +0400

    test

--- daily_challenge/lib/use_to_wigets/ListView/use_listView.dart
+++ daily_challenge/lib/use_to_wigets/ListView/use_listView.dart
@@ -18,18 +18,18 @@ class _ListViewPageState extends State<ListViewPage> {
           padding: const EdgeInsets.all(8),
           children: <Widget>[
             Container(
-              height: 50,
+              height: 40,
               color: Colors.amber[600],
               child: const Center(child: Text('Entry A')),
             ),
             Container(
-              height: 50,
+              height: 40,
               color: Colors.amber[500],
               child: const Center(child: Text('Entry B')),
             ),
             Container(
-              height: 50,
-              color: Colors.amber[100],
+              height: 40,
+              color: Colors.amber[200],
               child: const Center(child: Text('Entry C')),
             ),
           ],
