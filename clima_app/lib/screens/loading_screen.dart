e349071513a2cffe3219fbbe7384ec92edea16c8
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-01 17:27:25 +0400

    test

--- clima_app/lib/screens/loading_screen.dart
+++ clima_app/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:clima_app/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -14,13 +15,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    try {
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.low);
-      print(position);
-    } catch (e) {
-      print(e);
-    }
+    Location location=Location();
+    location.getCurrentLocation();
+
   }
 
   @override
