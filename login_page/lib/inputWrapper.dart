4e6ecf2ab87f23aace1d57bb41d8d290cb1b50b5
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-04 19:12:05 +0400

    test

--- login_page/lib/inputWrapper.dart
+++ login_page/lib/inputWrapper.dart
@@ -10,7 +10,18 @@ class inputWrapper extends StatelessWidget {
           SizedBox(
             height: 40,
           ),
-          Container(decoration: BoxDecoration(color: Colors.white),)
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(10)),
+            child: inputField(),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Text(
+            'Forget password?',
+            style: TextStyle(color: Colors.grey),
+          )
         ],
       ),
     );
