fab2e9e9552d0890d611963a9d784aaf7140978d
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-05 09:00:01 +0400

    test

--- login_page/lib/InputField.dart
+++ login_page/lib/InputField.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+
 class InputField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration:
+          BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey))),
+      child: TextField(
+        decoration: InputDecoration(
+            hintText: 'Enter your email',
+            hintStyle: TextStyle(color: Colors.grey)),
+      ),
+    );
   }
 }
