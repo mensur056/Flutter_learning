311d625d38c2aad04080a8de6b6600db3fcc5b80
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-06 09:10:00 +0400

    test

--- plant_app/lib/components/body.dart
+++ plant_app/lib/components/body.dart
@@ -8,12 +8,38 @@ class Body extends StatelessWidget {
     return Column(
       children: [
         Container(
-          height: size.height * 0.2,
+          height: size.height * 0.3,
           child: Stack(
             children: [
               Container(
-                height: size.height * 0.2 - 27,
-                decoration: BoxDecoration(color: kPrimaryColor,borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30),bottomRight: Radius.circular(30))),
+                height: size.height * 0.3 - 27,
+                decoration: BoxDecoration(
+                  color: kPrimaryColor,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 0,
+                left: 0,
+                bottom: 0,
+                child: Container(
+                  height: 54,
+                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(0.23),
+                      ),
+                    ],
+                  ),
+                ),
               )
             ],
           ),
