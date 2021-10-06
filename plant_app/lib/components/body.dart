2882e9cd88aa9f071bbee50e9e687253d48cc067
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-06 08:57:09 +0400

    test

---
+++ plant_app/lib/components/body.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        Container(
+          height: size.height * 0.2,
+          child: Stack(
+            children: [
+              Container(
+                height: size.height * 0.2 - 27,
+                decoration: BoxDecoration(color: kPrimaryColor),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
--- plant_app/lib/screens/home_screen.dart
+++ plant_app/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plant_app/components/body.dart';
 import 'package:plant_app/constants.dart';
 
 
@@ -7,12 +8,18 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        leading: IconButton(splashColor:kPrimaryColor,highlightColor: kPrimaryColor,
-          icon: SvgPicture.asset('assets/icons/menu.svg'),
-          onPressed: () {},
-        ),
+      appBar: buildAppBar(),body: Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(
+        splashColor: kPrimaryColor,
+        highlightColor: kPrimaryColor,
+        icon: SvgPicture.asset('assets/icons/menu.svg'),
+        onPressed: () {},
       ),
     );
   }
