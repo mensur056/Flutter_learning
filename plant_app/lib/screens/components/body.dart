d36c15be5210598690e06633341d2a0d3f80c1d5
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-07 08:58:39 +0400

    test

--- plant_app/lib/screens/components/body.dart
+++ plant_app/lib/screens/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plant_app/constants.dart';
+import 'package:plant_app/screens/components/title_with_more_btn.dart';
 import 'header_with_searchbox.dart';
 
 class Body extends StatelessWidget {
@@ -14,71 +15,6 @@ class Body extends StatelessWidget {
           TitleWithMoreBtn(
             title: 'Recomended',
             press: () {},
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class TitleWithMoreBtn extends StatelessWidget {
-  const TitleWithMoreBtn({required this.title, required this.press});
-
-  final String title;
-  final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      child: Row(
-        children: [
-          TitleWithCustomUnder(
-            text: title,
-          ),
-          Spacer(),
-          FlatButton(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20)),
-              color: kPrimaryColor,
-              onPressed: press,
-              child: Text(
-                'More',
-                style: TextStyle(color: Colors.white),
-              ))
-        ],
-      ),
-    );
-  }
-}
-
-class TitleWithCustomUnder extends StatelessWidget {
-  const TitleWithCustomUnder({required this.text});
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 24,
-      child: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
-            child: Text(
-              text,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Positioned(
-            left: 20,
-            right: 0,
-            bottom: 0,
-            child: Container(
-              color: kPrimaryColor,
-              height: 1,
-              margin: EdgeInsets.only(right: kDefaultPadding),
-            ),
           )
         ],
       ),
