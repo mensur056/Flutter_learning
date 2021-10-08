0f8a2725309f0c98276e98bb8844dcd4833abe3a
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-08 09:41:35 +0400

    test

--- plant_app/lib/screens/components/body.dart
+++ plant_app/lib/screens/components/body.dart
@@ -17,9 +17,27 @@ class Body extends StatelessWidget {
             press: () {},
           ),
           Container(
+            margin: EdgeInsets.only(
+                left: kDefaultPadding,
+                top: kDefaultPadding,
+                bottom: kDefaultPadding * 2.5),
             width: size.width * 0.4,
             child: Column(
-              children: [Image.asset('assets/images/image_1.png')],
+              children: [
+                Image.asset('assets/images/image_1.png'),
+                Container(width: 158,
+                  padding: EdgeInsets.all(kDefaultPadding / 2),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 50,
+                          color: kPrimaryColor.withOpacity(0.23)),
+                    ],
+                  ),
+                )
+              ],
             ),
           )
         ],
