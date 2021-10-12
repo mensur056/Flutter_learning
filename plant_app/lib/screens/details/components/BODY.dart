679c0b042df6b537c20d54b902c12ad595ca5135
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-12 15:11:59 +0400

    test

--- plant_app/lib/screens/details/components/BODY.dart
+++ plant_app/lib/screens/details/components/BODY.dart
@@ -8,36 +8,45 @@ class BODY extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Column(
       children: [
-        Row(
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  IconButton(
-                      onPressed: () {Navigator.pop(context);},
-                      icon: SvgPicture.asset('assets/icons/back_arrow.svg'))
-                ],
-              ),
-            ),
-            Container(
-              height: size.height * 0.8,
-              width: size.width * 0.6,
-              decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 60,
-                        color: kPrimaryColor.withOpacity(0.29))
+        SizedBox(
+          height: size.height * 0.8,
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon:
+                              SvgPicture.asset('assets/icons/back_arrow.svg')),
+                    ),Spacer()
                   ],
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(60),
-                      bottomLeft: Radius.circular(60)),
-                  image: DecorationImage(
-                      alignment: Alignment.centerLeft,
-                      fit: BoxFit.cover,
-                      image: AssetImage('assets/images/img.png'))),
-            )
-          ],
+                ),
+              ),
+              Container(
+                height: size.height * 0.8,
+                width: size.width * 0.6,
+                decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 60,
+                          color: kPrimaryColor.withOpacity(0.29))
+                    ],
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(60),
+                        bottomLeft: Radius.circular(60)),
+                    image: DecorationImage(
+                        alignment: Alignment.centerLeft,
+                        fit: BoxFit.cover,
+                        image: AssetImage('assets/images/img.png'))),
+              )
+            ],
+          ),
         ),
       ],
     );
