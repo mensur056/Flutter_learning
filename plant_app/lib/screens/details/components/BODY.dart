211a1a3748bb7ed3f81ebfaee75696a60ecc21ed
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-12 15:08:36 +0400

    test

--- plant_app/lib/screens/details/components/BODY.dart
+++ plant_app/lib/screens/details/components/BODY.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plant_app/constants.dart';
 
 class BODY extends StatelessWidget {
   @override
@@ -10,13 +12,23 @@ class BODY extends StatelessWidget {
           children: [
             Expanded(
               child: Column(
-                children: [],
+                children: [
+                  IconButton(
+                      onPressed: () {Navigator.pop(context);},
+                      icon: SvgPicture.asset('assets/icons/back_arrow.svg'))
+                ],
               ),
             ),
             Container(
               height: size.height * 0.8,
               width: size.width * 0.6,
-              decoration: BoxDecoration(boxShadow: [BoxShadow(offset: Offset(0,10),blurRadius: 60)],
+              decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 60,
+                        color: kPrimaryColor.withOpacity(0.29))
+                  ],
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(60),
                       bottomLeft: Radius.circular(60)),
