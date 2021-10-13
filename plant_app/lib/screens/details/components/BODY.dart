7a391491c65763e42c54137a45f712bae5262db5
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-13 19:13:25 +0400

    test

--- plant_app/lib/screens/details/components/BODY.dart
+++ plant_app/lib/screens/details/components/BODY.dart
@@ -25,25 +25,7 @@ class BODY extends StatelessWidget {
                               SvgPicture.asset('assets/icons/back_arrow.svg')),
                     ),
                     Spacer(),
-                    Container(
-                      padding: EdgeInsets.all(kDefaultPadding / 2),
-                      margin: EdgeInsets.symmetric(vertical: 0.03),
-                      width: 65,
-                      height: 65,
-                      decoration: BoxDecoration(
-                        color: kBackgroundColor,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(offset: Offset(0, 10), blurRadius: 25),
-                          BoxShadow(
-                            color: Colors.white,
-                            blurRadius: 20,
-                            offset: Offset(-15, -15),
-                          ),
-                        ],
-                      ),
-                      child: SvgPicture.asset('assets/icons/sun.svg'),
-                    )
+                    IconCard()
                   ],
                 ),
               ),
@@ -69,6 +51,32 @@ class BODY extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class IconCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.all(kDefaultPadding / 2),
+      margin: EdgeInsets.symmetric(vertical: size.height * 0.03),
+      width: 65,
+      height: 65,
+      decoration: BoxDecoration(
+        color: kBackgroundColor,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(offset: Offset(0, 10), blurRadius: 25),
+          BoxShadow(
+            color: Colors.white,
+            blurRadius: 20,
+            offset: Offset(-15, -15),
+          ),
+        ],
+      ),
+      child: SvgPicture.asset('assets/icons/sun.svg'),
     );
   }
 }
