527b8f5c808409106a8a7cc2e8ec84791aa5db14
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-15 08:45:48 +0400

    test

--- clima_app/lib/screens/loading_screen.dart
+++ clima_app/lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:clima_app/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -12,9 +13,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     getLocation();
-
     super.initState();
-
   }
 
   void getLocation() async {
@@ -25,12 +24,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
-    http.Response response = await http.get(
-        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    http.Response response = await http.get(Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'));
     print(response.body);
-    if(response.statusCode==200){
-      String data=response.body;
-    }else{print(response.statusCode);}
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var longitude = jsonDecode(data)['coord']['lon'];
+      print(longitude);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
