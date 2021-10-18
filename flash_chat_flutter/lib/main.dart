d24153401b2d9606bb466554dbb07a9e8533ced8
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-18 16:44:14 +0400

    test

--- flash_chat_flutter/lib/main.dart
+++ flash_chat_flutter/lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:flash_chat_flutter/screens/login_screen.dart';
+import 'package:flash_chat_flutter/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat_flutter/screens/welcome_screen.dart';
-
+import 'package:flash_chat_flutter/screens/chat_screen.dart';
 
 void main() => runApp(FlashChat());
 
@@ -13,7 +15,13 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute:,
+      routes: {
+        'welcome_screen': (context) => WelcomeScreen(),
+        'login_screen': (context) => LoginScreen(),
+        'registration': (context) => RegistrationScreen(),
+        'chat_screen':(context)=> ChatScreen()
+      },
     );
   }
 }
