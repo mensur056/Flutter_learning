70ca2ce4d11c577821ce1fcb0a498889fb76540b
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-21 09:18:57 +0400

    test

--- flash_chat_flutter/lib/screens/welcome_screen.dart
+++ flash_chat_flutter/lib/screens/welcome_screen.dart
@@ -20,15 +20,10 @@ class _WelcomeScreenState extends State<WelcomeScreen>
     super.initState();
     controller =
         AnimationController(vsync: this, duration: Duration(seconds: 2));
-    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
+    animation =
+        ColorTween(begin: Colors.red, end: Colors.blue).animate(controller);
     controller.forward();
-    animation.addStatusListener((status) {
-     if (status == AnimationStatus.completed){
-       controller.reverse(from: 1.0);
-     }else if(status==AnimationStatus.dismissed){
-       controller.forward();
-     }
-    });
+
     controller.addListener(() {
       setState(() {});
       print(animation.value);
@@ -36,9 +31,15 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   }
 
   @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -51,7 +52,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   tag: 'logo.flash',
                   child: Container(
                     child: Image.asset('images/logo.flash.png'),
-                    height: animation.value * 100,
+                    height: 60.0,
                   ),
                 ),
                 Text(
