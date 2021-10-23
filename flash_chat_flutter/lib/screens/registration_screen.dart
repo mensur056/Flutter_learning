63f1f6960318692f6aff246cc374dbfca59049b9
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-23 10:03:05 +0400

    test

--- flash_chat_flutter/lib/screens/registration_screen.dart
+++ flash_chat_flutter/lib/screens/registration_screen.dart
@@ -34,7 +34,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               onChanged: (value) {
                 //Do something with the user input.
               },
-              decoration: kTextFieldDecoration
+              decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your email')
             ),
             SizedBox(
               height: 8.0,
