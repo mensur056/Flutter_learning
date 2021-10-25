e1a6ee8b5c2373a6dfc11ff2c45f18319267c771
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-25 13:44:03 +0400

    test

---
+++ my_folder/lib/my_folder.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class MyFolder extends StatefulWidget {
+  @override
+  _MyFolderState createState() => _MyFolderState();
+}
+
+class _MyFolderState extends State<MyFolder> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.more),
+      ),
+    );
+  }
+}
