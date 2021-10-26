9d5658ce639becb2e9df65d8cfa9b192b40c0a03
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-26 09:08:08 +0400

    test

--- file_manager/lib/Item/Items.dart
+++ file_manager/lib/Item/Items.dart
@@ -10,7 +10,7 @@ class _ItemsState extends State<Items> {
   Widget build(BuildContext context) {
     return Container(
       height: 150,
-      color: Colors.white,
+      color: Colors.black,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -23,17 +23,19 @@ class _ItemsState extends State<Items> {
 }
 
 class Icons extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Container(
-          decoration: BoxDecoration(
-              color: Colors.blue, borderRadius: BorderRadius.circular(25)),
-          height: 50,
-          width: 50,
+        Padding(
+          padding: const EdgeInsets.only(left: 30),
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.blue, borderRadius: BorderRadius.circular(25)),
+            height: 50,
+            width: 50,
+          ),
         ),
         Container(
           decoration: BoxDecoration(
@@ -47,12 +49,15 @@ class Icons extends StatelessWidget {
           height: 50,
           width: 50,
         ),
-        Container(
-          decoration: BoxDecoration(
-              color: Colors.blue, borderRadius: BorderRadius.circular(25)),
-          height: 50,
-          width: 50,
-        )
+        Padding(
+          padding: const EdgeInsets.only(right: 30),
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.blue, borderRadius: BorderRadius.circular(25)),
+            height: 50,
+            width: 50,
+          ),
+        ),
       ],
     );
   }
