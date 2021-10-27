fbcd7e153e939711352e8d2c284aab68b6de0aff
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-27 11:32:56 +0400

    test

--- folder_manager/lib/apps/Apps.dart
+++ folder_manager/lib/apps/Apps.dart
@@ -11,21 +11,51 @@ class _AppsState extends State<Apps> {
     return Container(
       height: 150,
       color: Colors.black,
-      child: Row(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            color: Colors.black,
-            child: Image.asset('images/whtps.png'),
-            width: 20,
-            height: 20,
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white),
+                  child: Image.asset('images/whtps.png'),
+                  width: 20,
+                  height: 20,
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                'WhatsApp',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.w300),
+              ),
+              Spacer(),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.arrow_forward_ios_outlined,
+                    size: 10,
+                    color: Colors.grey,
+                  ))
+            ],
           ),
           SizedBox(
-            width: 10,
+            height: 15,
           ),
-          Text(
-            'WhatsApp',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w300),
-          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Container(
+              height: 90,
+              width: 90,
+              color: Colors.white,
+            ),
+          )
         ],
       ),
     );
