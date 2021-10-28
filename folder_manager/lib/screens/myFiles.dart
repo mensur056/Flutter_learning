95a33ba8cd621ee050de7265fd22011e39b82662
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-28 09:16:19 +0400

    test

--- folder_manager/lib/screens/myFiles.dart
+++ folder_manager/lib/screens/myFiles.dart
@@ -26,10 +26,23 @@ class _MyFilesState extends State<MyFiles> {
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(decoration: BoxDecoration(shape: BoxShape.circle,color: Colors.green),
-                  width: 60,
-                  height: 60,
-
+                Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: Colors.orange),
+                      width: 60,
+                      height: 60,
+                    ),
+                    Positioned(top: 5,left:5,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.black),
+                        width: 50,
+                        height: 50,
+                      ),
+                    )
+                  ],
                 ),
                 Padding(
                   padding: const EdgeInsets.only(top: 16, left: 8),
@@ -48,18 +61,26 @@ class _MyFilesState extends State<MyFiles> {
                 Spacer(),
                 IconButton(
                     onPressed: () {},
-                    icon: Icon(Icons.arrow_forward_ios_outlined,color: Colors.grey,size: 10,))
+                    icon: Icon(
+                      Icons.arrow_forward_ios_outlined,
+                      color: Colors.grey,
+                      size: 10,
+                    ))
               ],
             ),
-          ),Divider(height: 30,color: Colors.grey,),
+          ),
+          Divider(
+            height: 30,
+            color: Colors.grey,
+          ),
           Padding(
             padding: const EdgeInsets.only(left: 23),
             child: Row(
               children: [
                 Text(
                   'Internal shared storage >',
-                  style:
-                      TextStyle(color: Colors.grey, fontWeight: FontWeight.w100),
+                  style: TextStyle(
+                      color: Colors.grey, fontWeight: FontWeight.w100),
                 )
               ],
             ),
