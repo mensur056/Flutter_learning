ef1de73ad15306b3ec4ee07ffebc9e05ede72a86
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-29 09:20:00 +0400

    test

--- folder_manager/lib/screens/Files/Directory.dart
+++ folder_manager/lib/screens/Files/Directory.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Directory extends StatefulWidget {
-
   @override
   _DirectoryState createState() => _DirectoryState();
 }
@@ -9,6 +8,32 @@ class Directory extends StatefulWidget {
 class _DirectoryState extends State<Directory> {
   @override
   Widget build(BuildContext context) {
-    return Column(children: [],);
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              height: 60,
+              width: 60,
+              child: Image.asset(
+                'images/file.png',
+              ),
+            ),
+            Column(
+              children: [
+                Text(
+                  'Android',
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text('3 items | 19/08/2021  12:32',
+                    style: TextStyle(color: Colors.white))
+              ],
+            ),
+            IconButton(
+                onPressed: () {}, icon: Icon(Icons.arrow_forward_ios_outlined))
+          ],
+        )
+      ],
+    );
   }
 }
