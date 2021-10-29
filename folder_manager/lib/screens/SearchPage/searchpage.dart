9ea3db535a2883e32a6ef59ee553ab8d5e5188c9
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-10-29 15:52:30 +0400

    test

--- folder_manager/lib/screens/SearchPage/searchpage.dart
+++ folder_manager/lib/screens/SearchPage/searchpage.dart
@@ -6,24 +6,20 @@ class SearchPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: Container(
-            width: double.infinity,
-            height: 40,
-            decoration: BoxDecoration(
-                color: Colors.grey, borderRadius: BorderRadius.circular(5)),
-            child: Center(
-              child: Row(
-                children: [
-                  IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-                  TextField(
-                    decoration: InputDecoration(
-                        hintText: 'Search...', border: InputBorder.none),
-                  ),
-                ],
-              ),
+        backgroundColor: Colors.black,
+        title: Container(
+          width: double.infinity,
+          height: 40,
+          decoration: BoxDecoration(
+              color: Colors.grey, borderRadius: BorderRadius.circular(5)),
+          child: Center(
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: 'Search...', border: InputBorder.none),
             ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
