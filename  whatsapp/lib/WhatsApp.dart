82a92e69abe032bc278c8be3cbee95fdd7332342
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-11-01 13:00:05 +0400

    test

---  whatsapp/lib/WhatsApp.dart
+++  whatsapp/lib/WhatsApp.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
-class WhatsApp extends StatelessWidget {
 
+class WhatsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        backgroundColor: Colors.blueGrey,
+        leading: Row(
+          children: [Text('WhatsApp')],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
   }
 }
