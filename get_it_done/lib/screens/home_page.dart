35e906b899c27e4dd76ccce3583e7a9479415b9d
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-11-01 10:13:52 +0400

    test

--- get_it_done/lib/screens/home_page.dart
+++ get_it_done/lib/screens/home_page.dart
@@ -5,6 +5,20 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Get It Done'),
+      ),
+      body: Column(
+        children: [
+          Container(),
+          Container(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
