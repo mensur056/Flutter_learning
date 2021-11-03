2b7713f413d70839295e99400f3e695daf2953bb
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-11-03 15:08:37 +0400

    test

--- get_it_done/lib/models/items_data.dart
+++ get_it_done/lib/models/items_data.dart
@@ -3,10 +3,12 @@ import 'item.dart';
 
 class ItemData with ChangeNotifier {
   final List<Item> items = [
-    Item(title: 'Pazara git'),
-    Item(title: 'Elma al'),
-    Item(title: 'Eve gel'),
-    Item(title: '2 Saat uyu'),
+    Item(title: 'You should go to the store'),
+    Item(title: 'You should buy apples'),
+    Item(title: 'Go back'),
+    Item(title: 'You sleep 2 hours'),
+    Item(title: 'Finish'),
+
   ];
 
   void toggleStatus(int index) {
