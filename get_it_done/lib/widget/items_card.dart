47cc5174ca169e5237850161b99eb3b69b8a4859
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-11-04 09:54:36 +0400

    test

--- get_it_done/lib/widget/items_card.dart
+++ get_it_done/lib/widget/items_card.dart
@@ -13,13 +13,13 @@ class ItemCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 5,
+      elevation: isDone?1:5,
       shadowColor: Theme.of(context).primaryColor,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20),
       ),
       child: ListTile(
-        title: Text(title,style: TextStyle(color: Colors.black),),
+        title: Text(title,style: TextStyle(color: Colors.black,decoration: isDone?TextDecoration.lineThrough:null),),
         trailing: Checkbox(
           onChanged:toggle,
           value: isDone,
