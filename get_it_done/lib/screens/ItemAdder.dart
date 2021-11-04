4535ab66bd5fb50602c70556c99e60f481f8b508
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-11-04 19:46:58 +0400

    test

--- get_it_done/lib/screens/ItemAdder.dart
+++ get_it_done/lib/screens/ItemAdder.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
 class ItemAdder extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            TextField(style: TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: '...',
+                  labelText: 'Add Item'),
+              autofocus: true,
+            ),
+            FlatButton(onPressed: () {},color: Theme.of(context).accentColor, child: Text('add'))
+          ],
+        ),
+      ),
+    );
   }
 }
