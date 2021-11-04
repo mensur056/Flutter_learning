df28faa673509ffb3425fda8955f39569646fce9
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-11-04 19:53:53 +0400

    test

--- get_it_done/lib/screens/ItemAdder.dart
+++ get_it_done/lib/screens/ItemAdder.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ItemAdder extends StatelessWidget {
+  final textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -9,14 +11,22 @@ class ItemAdder extends StatelessWidget {
         padding: const EdgeInsets.all(12.0),
         child: Column(
           children: [
-            TextField(style: TextStyle(color: Colors.black),
+            TextField(
+              controller: textController,
+              onChanged: (input) {
+                print(textController.text);
+              },
+              style: TextStyle(color: Colors.black, fontSize: 20),
               decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: '...',
                   labelText: 'Add Item'),
               autofocus: true,
             ),
-            FlatButton(onPressed: () {},color: Theme.of(context).accentColor, child: Text('add'))
+            FlatButton(
+                onPressed: () {},
+                color: Theme.of(context).accentColor,
+                child: Text('Add'))
           ],
         ),
       ),
