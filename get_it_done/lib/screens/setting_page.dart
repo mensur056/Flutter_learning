6f682050b7f58ef7d0f7514bce85632662935767
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-11-06 17:07:41 +0400

    test

--- get_it_done/lib/screens/setting_page.dart
+++ get_it_done/lib/screens/setting_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-class SettingsPage extends StatelessWidget {
 
+class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        title: Text('Temanizi secin'),
+      ),body: Card(),
+    );
   }
 }
