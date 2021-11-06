72f31f498af5080ea5103eac8d83f4d6f65287f1
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-11-06 17:34:28 +0400

    test

--- get_it_done/lib/main.dart
+++ get_it_done/lib/main.dart
@@ -28,3 +28,27 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+ThemeData redTheme = ThemeData(
+  primaryColor: Colors.red,
+  accentColor: Colors.red,
+  scaffoldBackgroundColor: Colors.red,
+  appBarTheme: AppBarTheme(color: Colors.red),
+  primarySwatch: Colors.red,
+  textTheme: TextTheme(
+    subtitle1: TextStyle(color: Colors.white),
+    headline3: TextStyle(color: Colors.white),
+  ),
+);
+
+ThemeData greenTheme = ThemeData(
+  primaryColor: Colors.green,
+  accentColor: Colors.green,
+  scaffoldBackgroundColor: Colors.green,
+  appBarTheme: AppBarTheme(color: Colors.green),
+  primarySwatch: Colors.green,
+  textTheme: TextTheme(
+    subtitle1: TextStyle(color: Colors.white),
+    headline3: TextStyle(color: Colors.white),
+  ),
+);
