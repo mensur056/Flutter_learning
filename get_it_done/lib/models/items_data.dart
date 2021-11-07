1f6771954b91042c21bca1b3cadc228618710d0b
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-11-07 16:58:09 +0400

    test

--- get_it_done/lib/models/items_data.dart
+++ get_it_done/lib/models/items_data.dart
@@ -2,26 +2,28 @@ import 'package:flutter/material.dart';
 import 'item.dart';
 
 class ItemData with ChangeNotifier {
-  final List<Item> items = [
+  final List<Item> _items = [
     Item(title: 'You go store'),
     Item(title: 'You buy apple'),
     Item(title: 'Go back')
   ];
 
   void toggleStatus(int index) {
-    items[index].trueFalse();
+    _items[index].trueFalse();
     notifyListeners();
   }
 
   void addItem(String title) {
-    items.add(Item(title: title));
+    _items.add(Item(title: title));
     notifyListeners();
   }
 
   void deleteItem(int index) {
-    items.removeAt(index);
+    _items.removeAt(index);
     notifyListeners();
   }
+
+  List<Item> get items =>_items;
 }
 //Item(title: 'You go store'),
 //Item(title: 'You buy apple'),
