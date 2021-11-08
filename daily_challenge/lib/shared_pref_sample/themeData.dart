048dc59dd3721c12df0aaf0175eab74b0142ef73
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-11-08 18:11:00 +0400

    test

--- daily_challenge/lib/shared_pref_sample/themeData.dart
+++ daily_challenge/lib/shared_pref_sample/themeData.dart
@@ -10,8 +10,16 @@ ThemeData red = ThemeData(
     primarySwatch: Colors.red,
     scaffoldBackgroundColor: Colors.red.shade50);
 
-class ThemeColorData with ChangeNotifier{
-  bool isGreen=true;
+class ThemeColorData with ChangeNotifier {
+  bool _isGreen = true;
 
+  ThemeData get themeColor {
+    return _isGreen ? green : red;
+
+  }
+  void toggleColor(){
+    _isGreen=!_isGreen;
+    notifyListeners();
+  }
 
 }
