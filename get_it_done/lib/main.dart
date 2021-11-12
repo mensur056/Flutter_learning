bd87df899dd87f80d79b12e57de34d5cd1d0ac78
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-11-12 20:29:02 +0400

    test

--- get_it_done/lib/main.dart
+++ get_it_done/lib/main.dart
@@ -23,7 +23,11 @@ class MyApp extends StatelessWidget {
     // Provider.of<ColorThemeData>(context).loadThemeFromSharedPref();
     // Provider.of<ItemData>(context).loadItemsFromSharedPref();
     return Consumer2<ItemData, ColorThemeData>(
-      builder: (context, child, itemData, colorThemeData) {return MaterialApp(
+      builder: (context, itemsData, colorThemeData,child) {
+     itemsData.loadItemsFromSharedPref();
+     colorThemeData.loadThemeFromSharedPref();
+
+        return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: Provider.of<ColorThemeData>(context).selectedThemeData,
         home: HomePage(),
