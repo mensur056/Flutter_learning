079d675ab03990b05e0b8741941f0a58a94d17d4
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-11-13 19:30:53 +0400

    test

--- life_expectancy/lib/constants.dart
+++ life_expectancy/lib/constants.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-const kBottomContainerHeight = 80.0;
+const kBottomContainerHeight = 79.0;
 const kActiveCardColour = Color(0xFF1D1E33);
 const kInactiveCardColour = Color(0xFF111328);
 const kBottomContainerColour = Color(0xFFEB1555);
 const kNumberTextStyle = TextStyle(fontSize: 50, fontWeight: FontWeight.w900);
 
 const kLabelTextStyle = TextStyle(
-  fontSize: 18,
+  fontSize: 17,
   color: Color(0xFF8D8E98),
 );
 const kLargeButtonTextStyle =
