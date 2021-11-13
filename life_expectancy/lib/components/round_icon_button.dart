4a5b9073febe3042653ee8fca9e7177d05d2febf
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-11-13 19:29:32 +0400

    test

--- life_expectancy/lib/components/round_icon_button.dart
+++ life_expectancy/lib/components/round_icon_button.dart
@@ -14,7 +14,7 @@ class RoundIconButton extends StatelessWidget {
         child: Icon(icon),
         onPressed: onPressed,
         elevation: 0,
-        constraints: BoxConstraints.tightFor(width: 44, height: 44),
+        constraints: BoxConstraints.tightFor(width: 45, height: 45),
         shape: CircleBorder(),
         fillColor: Color(0xFF4C4F5E));
   }
