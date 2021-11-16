b2e5d4e29b9e28c6914820b5f4a619dbbdbc8119
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-11-16 20:14:19 +0400

    test

--- first_firebase_project/lib/main.dart
+++ first_firebase_project/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  Firebase.initializeApp();
+void main() async{
+
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
