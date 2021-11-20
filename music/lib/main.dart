6dcef7957ff9d2eff9ef9389269fe6f065418cb7
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-11-20 21:39:02 +0400

    test

--- music/lib/main.dart
+++ music/lib/main.dart
@@ -11,54 +11,66 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Container(
-            width: double.infinity,
-            height: 40,
-            decoration: BoxDecoration(
-                color: Color(0xFFc8c8c8),
-                borderRadius: BorderRadius.circular(5)),
-            child: Center(
-              child: TextField(
-                decoration: InputDecoration(
-                    icon: Icon(
-                      Icons.search,
-                      color: Colors.blue,
-                    ),
-                    border: InputBorder.none),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 40,
+              decoration: BoxDecoration(
+                  color: Color(0xFFc8c8c8),
+                  borderRadius: BorderRadius.circular(5)),
+              child: Center(
+                child: TextField(
+                  decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.search,
+                        color: Colors.blue,
+                      ),
+                      border: InputBorder.none),
+                ),
               ),
             ),
-          ),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            SingleChildScrollView(
+              child: Column(
                 children: [
-                  Column(
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.access_time_outlined)),Text('Recent')
-                    ],
-                  ),Column(
-                    children: [
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.favorite)),Text('Favorites')
-                    ],
-                  ),Column(
-                    children: [
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.library_music)),Text('Playlist')
+                      Padding(
+                        padding: const EdgeInsets.only(left: 50.0),
+                        child: Column(
+                          children: [
+                            IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.access_time_outlined)),
+                            Text('Recent')
+                          ],
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          IconButton(
+                              onPressed: () {}, icon: Icon(Icons.favorite)),
+                          Text('Favorites')
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 50.0),
+                        child: Column(
+                          children: [
+                            IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.library_music)),
+                            Text('Playlist')
+                          ],
+                        ),
+                      )
                     ],
                   )
                 ],
-              )
-            ],
-          ),
+              ),
+            ),
+          ],
         ),
       ),
     );
