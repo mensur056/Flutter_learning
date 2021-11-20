683237b6de33d3930b245057e9a42fcc6ed7d5bc
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-11-20 21:31:35 +0400

    test

--- music/lib/main.dart
+++ music/lib/main.dart
@@ -7,25 +7,41 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
-      home: Scaffold(backgroundColor: Colors.white,
-        appBar:AppBar(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
           backgroundColor: Colors.white,
           title: Container(
             width: double.infinity,
             height: 40,
             decoration: BoxDecoration(
-                color: Color(0xFFc8c8c8), borderRadius: BorderRadius.circular(5)),
+                color: Color(0xFFc8c8c8),
+                borderRadius: BorderRadius.circular(5)),
             child: Center(
               child: TextField(
                 decoration: InputDecoration(
-                    icon: Icon(Icons.search,color: Colors.blue,),
-
+                    icon: Icon(
+                      Icons.search,
+                      color: Colors.blue,
+                    ),
                     border: InputBorder.none),
               ),
             ),
           ),
         ),
+        body: SingleChildScrollView(
+            child: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(
+                    onPressed: () {}, icon: Icon(Icons.access_time_outlined))
+              ],
+            )
+          ],
+        )),
       ),
     );
   }
